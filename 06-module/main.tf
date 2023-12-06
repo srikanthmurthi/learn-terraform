fc6e45aa3bee87b5ae422fc7133bc6964db758ab
murthisrikanth <sreekanthnani222@gmail.com>
2023-12-06 13:11:04 +0530

    playbook

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,10 +1,4 @@
-module "sample1"{
+module "ec2" {
   source = "./ec2"
-  name = "sample1"
-}
-
-module "sample2"{
-  source = "./ec2"
-  name = "sample2"
 }
 
--- 07-module-1/ec2/main.tf
+++ 07-module-1/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "sample"
+    Name = var.name
   }
 }
 
@@ -16,7 +16,7 @@ data "aws_ami" "example" {
 
 
 resource "aws_security_group" "sg" {
-  name        = "sample"
+  name        = var.name
   description = "Allow TLS inbound traffic"
 
   ingress {
@@ -36,6 +36,12 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = "sample"
+    Name = var.name
   }
+}
+
+variable "name" {}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
