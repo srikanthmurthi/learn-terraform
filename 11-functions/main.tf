1b8d6f7adf81afd590f68488e2c4b87078b41562
murthisrikanth <sreekanthnani222@gmail.com>
2024-01-02 18:33:59 +0530

    playbook

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -6,3 +6,31 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruit_count"{
+  value = length(var.fruits)
+}
+
+variable "classes" {
+  default = {
+    devops = {
+      name = "devops"
+      topics = ["jenkins", "docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics" {
+  value = lookup(var.classes, "aws", null)
+}
