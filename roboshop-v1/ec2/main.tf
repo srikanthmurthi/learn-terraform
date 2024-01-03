85d741402efc5a99dfe6f84958c84b90fa48553b
murthisrikanth <sreekanthnani222@gmail.com>
2024-01-03 20:14:46 +0530

    playbook

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -27,43 +27,42 @@ resource "null_resource" "ansible" {
   }
 }
 
+resource "aws_route53_record" "www" {
+  zone_id = "Z055331734ICV430E01P7"
+  name    = "${var.name}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
 
-  resource "aws_route53_record" "www" {
-    zone_id = "Z0720150DPEERLK830UW"
-    name    = "${var.name}-dev"
-    type    = "A"
-    ttl     = 30
-    records = [aws_instance.web.private_ip]
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+
+resource "aws_security_group" "sg" {
+  name        = var.name
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  data "aws_ami" "example" {
-    owners      = ["973714476881"]
-    most_recent = true
-    name_regex  = "Centos-8-DevOps-Practice"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  resource "aws_security_group" "sg" {
-    name        = var.name
-    description = "Allow TLS inbound traffic"
-
-    ingress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-      Name = var.name
-    }
+  tags = {
+    Name = var.name
   }
-
+}
 
 variable "name" {}
